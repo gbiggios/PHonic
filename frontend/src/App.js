cbab7fa31e40abe326c98dfdf2b2ce204e432667
AlanEspinoza01 <lagunaverde004@gmail.com>
2024-12-19 12:22:07 -0300

    creando una pagina de descripción de artistas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
-
+import Artist_Information from "./pages/Artist_Information";
 import Home from "./pages/Home"; 
 import Login from './pages/Login';
 import Register from "./pages/Register";
@@ -15,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/Artist_Information" element={<Artist_Information />} />
       </Routes>
     </Router>
   );
